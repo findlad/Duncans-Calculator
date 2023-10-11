0ca0d191efba09a8dd5b3cf42f985dbb88201983
Findlad <findlad@gmail.com>
2023-10-11 15:05:38 -0600

    removed index

--- index.js
+++
@@ -1,9 +0,0 @@
-function greet(greeting, buttonId, newButtonLabel) {
-  let clickMeButton = document.getElementById(buttonId);
-  clickMeButton.innerHTML = newButtonLabel;
-  console.log(greeting, "C11!!");
-  clickMeButton.style.backgroundColor = "Red";
-  clickMeButton.style.color = "Black";
-  var sound = new Audio("playSound.wav");
-  sound.play();
-}
